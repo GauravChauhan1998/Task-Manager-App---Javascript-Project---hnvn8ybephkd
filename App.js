c1d33a6fa77bc16291ed2fcc59ea83d855865135
Gaurav Chauhan <gauravchauhan@Gauravs-MacBook-Air.local>
2023-06-20 15:53:19 +0530

    done with basic structure

--- App.js
+++ App.js
@@ -1,1 +1,9 @@
+const addTask = () => {
+    let view = document.getElementById("taskList");
+	let newTd = document.createElement("li");
+	var inputText = document.getElementById("name").value;
+	var node = document.createTextNode(inputText);
+	newTd.appendChild(node);
+	view.appendChild(newTd);
+}
 
