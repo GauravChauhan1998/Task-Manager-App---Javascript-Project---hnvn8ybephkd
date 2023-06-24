2a438e1d409fe93a0fba0e347efb378b4f60ae15
Gaurav Chauhan <gauravchauhan@Gauravs-MacBook-Air.local>
2023-06-24 17:22:56 +0530

    done

--- App.js
+++ App.js
@@ -1,9 +1,28 @@
-const addTask = () => {
-    let view = document.getElementById("taskList");
-	let newTd = document.createElement("li");
-	var inputText = document.getElementById("name").value;
-	var node = document.createTextNode(inputText);
-	newTd.appendChild(node);
-	view.appendChild(newTd);
-}
+const form = document.getElementById("todo-form");
+const input = document.getElementById("todo-input");
+const todoLane = document.getElementById("open");
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const value = input.value;
+
+	if(!value) return;
+
+	const newTask = document.createElement("p");
+	newTask.classList.add("task");
+	newTask.setAttribute("draggable", "true");
+	newTask.innerText = value;
+
+	newTask.addEventListener("dragstart", () => {
+		newTask.classList.add("is-dragging");
+	});
+
+	newTask.addEventListener("dragend", () => {
+		newTask.classList.remove("is-dragging");
+	});
+
+	todoLane.appendChild(newTask);
+
+	input.value = "";
+});
 
